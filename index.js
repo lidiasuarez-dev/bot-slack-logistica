48bab2d65669f3564d98b1396ef13ef15ebbfcf3
Justo <Justo@192.168.1.45>
2026-03-05 15:11:16 -0500

    fix slack events receiver

--- index.js
+++ index.js
@@ -2,42 +2,30 @@ require('dotenv').config();
 const { App, ExpressReceiver } = require('@slack/bolt');
 
 const receiver = new ExpressReceiver({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  receiver,
+  receiver
 });
 
 app.event('message', async ({ event, client }) => {
+
   if (event.bot_id) return;
+
   if (event.channel !== process.env.CANAL_PRINCIPAL) return;
 
   const texto = event.text || "";
 
   const marcas = [
-  "San Camilo",
-  "Barrita Burrito",
-  "Sushi Fans",
-  "Animal Cocina",
-  "Sierra Nevada",
-  "Juan Valdez",
-  "Chorizo Artesano",
-  "Lo Saldes",
-  "Just Burger",
-  "Sin Miedo",
-  "Shaka",
-  "Voraz",
-  "Mekong",
-  "Bangkok",
-  "Fatboy",
-  "Delirio",
-  "Tamashi Sushi",
-  "Gari Food",
-  "Gari Sushi",
-  "Burger Depot"
-];
+    "San Camilo", "Barrita Burrito", "Sushi Fans", "Animal Cocina",
+    "Sierra Nevada", "Juan Valdez", "Chorizo Artesano", "Lo Saldes",
+    "Just Burger", "Sin Miedo", "Shaka", "Voraz",
+    "Mekong", "Bangkok", "Fatboy", "Delirio",
+    "Tamashi Sushi", "Gari Food", "Gari Sushi",
+    "Burger Depot"
+  ];
 
   const contieneMarca = marcas.some(m =>
     texto.toLowerCase().includes(m.toLowerCase())
@@ -49,10 +37,9 @@ app.event('message', async ({ event, client }) => {
     channel: process.env.CANAL_EQUIPO,
     text: texto
   });
+
 });
 
-const PORT = process.env.PORT || 3000;
-
-receiver.app.listen(PORT, () => {
-  console.log(`⚡️ Servidor corriendo en puerto ${PORT}`);
+receiver.app.listen(process.env.PORT || 3000, () => {
+  console.log(`⚡️ Servidor corriendo en puerto ${process.env.PORT}`);
 });
